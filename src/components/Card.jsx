7049462add60b44f23f1d268c4084808cee92acb
CYTRIC48 <stroevkostya431@gmail.com>
2026-05-25 19:50:21 +0300

    images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
 import styles from "./Card.module.css";
+import Pingvin from "../../public/images/pingviny.jpg";
 
 export default function Card({ title, text, price }) {
   return (
-    <div>
+    <div className={styles.card}>
+      {/* <img className={styles.picture} src="/images/pingvin.jpg" alt="" /> */}
+      <img className={styles.picture} src={Pingvin} alt="" />
       <h2>{title}</h2>
       <p>{text}</p>
       <p>{price} $</p>
